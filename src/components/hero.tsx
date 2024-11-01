ccfd6aba5ce9494b6644fbeb1672c22fee3c94b0
aminebajji-veylinx <amine.bajji@etu.uae.ac.ma>
2024-11-01 19:13:03 +0100

    add leading-loose

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,7 +11,7 @@ export const Hero = () => {
       <div className="container grid lg:grid-cols-2 place-items-center pb-28 pt-20 md:pb-44 md:pt-20 gap-10">
         <div className="text-center lg:text-start space-y-6">
           <main className="text-5xl md:text-6xl font-bold">
-            <h1 className="inline">
+            <h1 className="inline leading-normal">
               <span className="inline text-white">نادي بصائر التربوي</span>
             </h1>
           </main>
